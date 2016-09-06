41182493f5a24276abaa7dc1172a28a742da2d13
Chongchong Wang <chongchong1110@gmail.com>
2016-09-05 23:35:49 -0700

    dashboard

---
+++ static/js/main.js
@@ -0,0 +1,37 @@
+var myApp = angular.module('myApp',['angular-svg-round-progressbar']);
+myApp.factory('mainFac',function(){
+	factory = {};
+
+	return factory;
+});
+myApp.controller('mainCon',function($scope,mainFac){
+	$scope.one = {
+		'a':0.1,
+		'b':0.8,
+		'c':0.3,
+		'weight':150,
+		'in':1000,
+		'out':800
+	};
+	$scope.two = {
+		'a':0.9,
+		'b':0.5,
+		'c':0.5,
+		'weight':180,
+		'in':1500,
+		'out':900
+	};
+	$scope.three = {
+		'a':0.3,
+		'b':0.6,
+		'c':0.8,
+		'weight':160,
+		'in':1250,
+		'out':1000
+	};
+	$scope.info=$scope.one;
+	$scope.change = function(day){
+		$scope.info = day;
+	};
+	
+});
